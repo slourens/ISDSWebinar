79d218129b3395702125dca03f31f0ae3ea9251a
Spencer Lourens <slourens@iu.edu>
2019-03-06 14:22:32 -0500

    Added second example for extracting Health Behavior data, scripting this across all Indiana counties.

--- RSeleniumExample.R
+++ RSeleniumExample.R
@@ -11,6 +11,7 @@ library(purrr)
 ## for some string manipulations - cleaning up scraped data
 library(stringr)
 ## for wrangling data - using mutate_at(), etc.
+library(dplyr)
 
 ## create remote driver server and client - using "chrome"
 remDr <- rsDriver(browser = "chrome")
@@ -76,10 +77,12 @@ stateSelectLink <- rD$findElement(using = "css", "#app-main > div.app-header.bre
 stateSelectLink$clickElement()
 
 ## click the div for the state we want:
-rD$executeScript("jQuery('#ui-select-choices-1 > li:contains(\"Nebraska\")').click();")
+rD$executeScript("jQuery('#ui-select-choices-1 > li:contains(\"Indiana\")').click();")
 
 ## select county select input
 countySelectLink <- rD$findElement(using = "css", "#main > div.ng-scope > div > a")
+
+countySelectLink <- rD$findElement(using = "css", "#app-wrapper > div.app-header.nav.clearfix.ng-scope > div:nth-child(2) > div > a")
 
 ## make search box visible
 countySelectLink$clickElement()
@@ -87,7 +90,7 @@ countySelectLink$clickElement()
 ## change value and update
 
 ## click the div for the county we want - more JS:
-rD$executeScript("jQuery('#ui-select-choices-3 > li:contains(\"Adams\")').click();")
+rD$executeScript("jQuery('#ui-select-choices-3 > li:contains(\"Marion\")').click();")
 
 ## now grab demographic again:
 
@@ -140,7 +143,6 @@ extractDemographics <- function(state = "Indiana", county = "Marion")
   ## this link shows up one of two ways - try both
   i <- 1
   
-  
   while (identical(countySelectLink, "not found!"))
   {
     Sys.sleep(1)
@@ -154,6 +156,7 @@ extractDemographics <- function(state = "Indiana", county = "Marion")
         rD$findElement(using = "css", "#app-wrapper > div.app-header.nav.clearfix.ng-scope > div:nth-child(2) > div > a")}, 
         error = function(e) {"not found!"})      
     }
+    i <- i + 1
   }
   
   ## make search box visible
@@ -162,7 +165,13 @@ extractDemographics <- function(state = "Indiana", county = "Marion")
   ## change value and update
   
   ## click the div for the county we want - more JS:
+  
+  ## Notice - this link can also change, i.e. the appended value at the end
+  ## try all up to 4
+  rD$executeScript(paste0("jQuery('#ui-select-choices > li:contains(\"", county, "\")').click();"))
   rD$executeScript(paste0("jQuery('#ui-select-choices-2 > li:contains(\"", county, "\")').click();"))
+  rD$executeScript(paste0("jQuery('#ui-select-choices-3 > li:contains(\"", county, "\")').click();"))
+  rD$executeScript(paste0("jQuery('#ui-select-choices-4 > li:contains(\"", county, "\")').click();"))
   
   ## now grab demographic again:
   Sys.sleep(1)
@@ -207,6 +216,155 @@ extractDemographics("Indiana", "Marion")
 extractDemographics("Indiana", "Martin")
 extractDemographics("Alabama", "Barbour")
 
+## now, scale it up:
+## let's extract all counties from Indiana from the web page, then iterate:
+## if you already have all counties in your R session, you don't need this
+## I just want to show the process of using extractDemographics on all Indy counties - it works fairly quickly!
+rD$navigate("http://www.countyhealthrankings.org/app/texas/2018/rankings/marion/county/outcomes/overall/snapshot")
+
+stateSelectLink <- rD$findElement(using = "css", "#app-main > div.app-header.breadcrumb > div > div.app-state-select.ui-select-container.select2.select2-container.ng-empty.ng-valid > a")
+
+## make search box visible
+stateSelectLink$clickElement()
+
+## click the div for the state we want:
+rD$executeScript(paste0("jQuery('#ui-select-choices-1 > li:contains(\"Indiana\")').click();"))
+
+Sys.sleep(1)
+## select county select input
+## need error catching here
+countySelectLink <- tryCatch({
+  rD$findElement(using = "css", "#app-wrapper > div.app-header.nav.clearfix.ng-scope > div:nth-child(2) > div > a")}, 
+  error = function(e) {"not found!"})
+
+## this link shows up one of two ways - try both
+i <- 1
+
+while (identical(countySelectLink, "not found!"))
+{
+  Sys.sleep(1)
+  if (i %% 2 == 1)
+  {
+    countySelectLink <- tryCatch({
+      rD$findElement(using = "css", "#main > div.ng-scope > div > a")
+    }, error = function(e) {"not found!"})      
+  } else {
+    countySelectLink <- tryCatch({
+      rD$findElement(using = "css", "#app-wrapper > div.app-header.nav.clearfix.ng-scope > div:nth-child(2) > div > a")}, 
+      error = function(e) {"not found!"})      
+  }
+  i <- i + 1
+}
+
+## make search box visible
+countySelectLink$clickElement()
+
+## extract all county names
+## remember the selector can be #ui-select-choices, #ui-select-choices-3, etc.
+counties <- rD$findElements(using = "css", "#ui-select-choices-4 > li > div > span")
+
+counties <- map_chr(counties, function(t) {t$getElementAttribute("innerHTML")[[1]]})
+
+## iterate over counties:
+dfList <- map(counties[1:5], function(t) {extractDemographics("Indiana", t)})
 
 ## Fun, but what next? We likely want more than demographics
+extractHB <- function(state = "Indiana", county = "Marion")
+{
+  rD$navigate("http://www.countyhealthrankings.org/app/texas/2018/rankings/marion/county/outcomes/overall/snapshot")
+  
+  stateSelectLink <- rD$findElement(using = "css", "#app-main > div.app-header.breadcrumb > div > div.app-state-select.ui-select-container.select2.select2-container.ng-empty.ng-valid > a")
+  
+  ## make search box visible
+  stateSelectLink$clickElement()
+  
+  ## click the div for the state we want:
+  rD$executeScript(paste0("jQuery('#ui-select-choices-1 > li:contains(\"", state, "\")').click();"))
+  
+  Sys.sleep(1)
+  ## select county select input
+  ## need error catching here
+  countySelectLink <- tryCatch({
+    rD$findElement(using = "css", "#app-wrapper > div.app-header.nav.clearfix.ng-scope > div:nth-child(2) > div > a")}, 
+    error = function(e) {"not found!"})
+  
+  ## this link shows up one of two ways - try both
+  i <- 1
+  
+  while (identical(countySelectLink, "not found!"))
+  {
+    Sys.sleep(1)
+    if (i %% 2 == 1)
+    {
+      countySelectLink <- tryCatch({
+        rD$findElement(using = "css", "#main > div.ng-scope > div > a")
+      }, error = function(e) {"not found!"})      
+    } else {
+      countySelectLink <- tryCatch({
+        rD$findElement(using = "css", "#app-wrapper > div.app-header.nav.clearfix.ng-scope > div:nth-child(2) > div > a")}, 
+        error = function(e) {"not found!"})      
+    }
+    i <- i + 1
+  }
+  
+  ## make search box visible
+  countySelectLink$clickElement()
+  
+  ## change value and update
+  
+  ## click the div for the county we want - more JS:
+  
+  ## Notice - this link can also change, i.e. the appended value at the end
+  ## try all up to 4
+  rD$executeScript(paste0("jQuery('#ui-select-choices > li:contains(\"", county, "\")').click();"))
+  rD$executeScript(paste0("jQuery('#ui-select-choices-2 > li:contains(\"", county, "\")').click();"))
+  rD$executeScript(paste0("jQuery('#ui-select-choices-3 > li:contains(\"", county, "\")').click();"))
+  rD$executeScript(paste0("jQuery('#ui-select-choices-4 > li:contains(\"", county, "\")').click();"))
+  
+  ## now grab demographic again:
+  Sys.sleep(1)
+  
+  ## notice that it takes time to load - can get an error if we're not careful!
+  HBtbody <- tryCatch(rD$findElement(using = "css", "#main-inner-wrapper > div.content.ng-scope > div > table > tbody.component-body.component-id-3-body"), error = function(e) {"not found!"})
+  
+  while(identical(HBtbody, "not found!"))
+  {
+    Sys.sleep(1)
+    HBtbody <- tryCatch(rD$findElement(using = "css", "#main-inner-wrapper > div.content.ng-scope > div > table > tbody.component-body.component-id-3-body"), error = function(e) {"not found!"})
+  }
+  
+  HBRows <- HBtbody$findChildElements(using = "css", "tr ")
+
+  ## 12 rows - confirm with webpage
+  
+  ## proccess a row
+  HBRowEntries <- map(HBRows[1:9], function(t) {
+    entries <- t$findChildElements(using = "css", "td")
+    ## first entry has a span and a element within it:
+    ## td 1, 3, and 7 are what we want
+    name <- entries[[1]]$findChildElement(using = "css", "a > span")$getElementAttribute("innerHTML")[[1]]
+    ## county is sometimes div > span (if no %), otherwise div > div
+    countyVal <- tryCatch({entries[[3]]$findChildElement(using = "css", 
+                                            "div > div")$getElementAttribute('innerHTML')[[1]]}, error = function(e) {"not found!"})
+    if (identical(countyVal, "not found!"))
+    {
+      countyVal <- tryCatch({entries[[3]]$findChildElement(using = "css", 
+                                                        "div > span")$getElementAttribute('innerHTML')[[1]]}, error = function(e) {"not found!"})
+    }
+    stateVal <- entries[[7]]$getElementAttribute('innerHTML')[[1]]
+    return(data.frame(header = name, county_value = countyVal, state = stateVal))
+  })
+  
+  ## create larger data.frame to hold results
+  dfHB <- do.call('rbind', HBRowEntries)
+  dfHB$county <- county
+  dfHB
+  
+}
+
+extractHB("Indiana", "Marion")
+extractHB("Indiana", "Hamilton")
+
+## again, iterate over all counties in Indiana:
+dfHBList <- map(counties[1:5], function(t) {extractHB("Indiana", t)})
 
